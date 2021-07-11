ca65b1db3f45313c1181ffc02c213779f1a1c9b7
Alvaro Martinez <amartinez@iaa.es>
2021-07-11 13:58:25 +0200

    This haved changed considerably. In order to aligned them you have to open the lists on Aladin locate a two common stars and copy and paste their X and Y coordinates on the variables xm_ref ym_ref for GNS star and xm and ym for Zoc stars. The code them aligns the two list with 5 loops of a degree1 pol. and 40 loops of a degree 2 poli. Tryed same conde with idl with same results, at aligme/Users/amartinez/Desktop/PhD/HAWK/The_Brick/photometry/WHOLE_im/pruebas_scripts_git/aligment_with_GNS.pro

--- 11_aligment_with_GNS.py
+++ 11_aligment_with_GNS.py
@@ -4,6 +4,7 @@
 # In[1]:
 
 
+
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy.table import QTable
@@ -19,8 +20,6 @@ from astropy.io import fits
 # In[2]:
 
 
-# como comprobacion has hecho este mismo scripts usando IDL con el metodo de los initial offset. El resultado es el mismo.
-# el script de IDL se llama aligment_with_GNS.pro
 band='H'
 #band='Ks'
 exptime=10
@@ -33,7 +32,7 @@ py_pruebas='/Users/amartinez/Desktop/PhD/HAWK/The_Brick/py_pruebas/'
 images='/Users/amartinez/Desktop/PhD/HAWK/The_Brick/07.1_Reduce_aligned/054_'+band+'/dit_'+str(exptime)+'/'+folder
 tmp='/Users/amartinez/Desktop/PhD/HAWK/The_Brick/photometry/054_'+band+'/dit_'+str(exptime)+'/'+folder+'tmp/'
 sirius='/Users/amartinez/Desktop/PhD/HAWK/The_Brick/SIRIUS/'
-GNS='/Users/amartinez/Desktop/PhD/HAWK/The_Brick/field_Bulge18/'
+GNS='/Users/amartinez/Desktop/PhD/HAWK/The_Brick/field12/'
 scripts='/Users/amartinez/Desktop/PhD/HAWK/The_Brick/photometry/scripts/'
 
 stream = open(scripts+'polywarp.py')
@@ -41,160 +40,86 @@ read_file = stream.read()
 exec(read_file)
 
 distancia=1
-#l_mag=0.01
-
-
-#slect apropiate columns from GNS
-if band =='H':
-    r_gns=8
-    d_gns=10
-    brillo=18
-if band=='Ks':
-    r_gns=12
-    d_gns=14
-    brillo=20
+chip=3# in this case only chip 2 and chip 3 have common elements with the GNS on the brick
 
 
 # In[3]:
 
 
-#galactic coordinates for GNS
-RA,DRA,DEC,DDEC,raJ,draJ,decJ,ddecJ,raH,draH,decH,ddecH,raKs,draKs,decKs,ddecKs,J,dJ,H,dH,Ks,dKs=np.loadtxt(GNS+'catalogue_F18_c3.txt',unpack=True)
-field18=np.loadtxt(GNS+'catalogue_F18_c3.txt')
-GNSH_gal = SkyCoord(ra=field18[:,0]*u.degree, dec=field18[:,2]*u.degree, frame='icrs').galactic
-t = QTable([GNSH_gal], names=['coord'])
-df=t.to_pandas()
-gal_GNS=df.to_numpy()
-field18=np.c_[field18,gal_GNS[:,0],gal_GNS[:,1]]
-#field18=RA,DRA,DEC,DDEC,raJ,draJ,decJ,ddecJ,raH,draH,decH,ddecH,raKs,draKs,decKs,ddecKs,J,dJ,H,dH,Ks,dKs,l,b
-l_max=max(field18[:,22])
-l_min=min(field18[:,22])
-b_max=max(field18[:,23])
-b_min=min(field18[:,23])
+x_gns, dx_gns, y_gns, dy_gns, raH, draH, decH, ddecH, mJ, dmJ, mH, dmH, mK, dmK=np.loadtxt(GNS+'field12_on_brick.txt',unpack=True)
+field12=np.loadtxt(GNS+'field12_on_brick.txt')
+valid_H=np.where(mH < 90)
+raH=raH[valid_H]
+decH=decH[valid_H]
+mH=mH[valid_H]
+x_gns=x_gns[valid_H]*0.5
+y_gns=y_gns[valid_H]*0.5
+field12[:,0]*=0.5
+field12[:,2]*=0.5
+gns_xy=np.array([x_gns,y_gns]).T
 
-#galactic coordinate for the Zoccaly to set them on field18
-fig,ax=plt.subplots(1,1,figsize=(20,10))
-dic_chips={}
-for chip in range(1,5):
-    #ra ,dec , m, dm, f, df,x,y,dx,dy= np.loadtxt(tmp+'stars_calibrated_%s_chip%s_sirius.txt'%(band,chip),unpack=True)
-    lst_1=np.loadtxt(tmp+'stars_calibrated_%s_chip%s_sirius.txt'%(band,chip),unpack=False)
-    print(len(lst_1),chip)
-    brick_gal = SkyCoord(ra=lst_1[:,0]*u.degree, dec=lst_1[:,1]*u.degree, frame='icrs').galactic
-    t = QTable([brick_gal], names=['coord'])
-    df=t.to_pandas()
-    gal_brick=df.to_numpy()
-    lst_1=np.c_[lst_1,gal_brick[:,0],gal_brick[:,1]]
-    #ra ,dec , m, dm, f, df,x,y,dx,dy,l,b
-    lst_1_comm=[lst_1[i] for i in range(len(lst_1)) if (l_min<lst_1[i,10]<l_max and b_min<lst_1[i,11]<b_max)]
-    dic_chips['chip_%s'%(chip)]=lst_1_comm
-    dic_chips['chip_%s'%(chip)]=np.array( dic_chips['chip_%s'%(chip)])
-    print('Chip %s galaticeado.'%(chip),len(dic_chips['chip_%s'%(chip)]))
-    f = fits.open(GNS+'field18_c3.fits')
-    w = WCS(f[0].header)
-    #print(w)
-    #extract coordinates X,Y within the frame of the image field18
-    x_gns,y_gns=w.world_to_pixel_values(dic_chips['chip_%s'%(chip)][:,0],dic_chips['chip_%s'%(chip)][:,1])
-    dic_chips['chip_%s'%(chip)][:,6]=x_gns
-    dic_chips['chip_%s'%(chip)][:,7]=y_gns
-    #field18= RA,DRA,DEC,DDEC,raJ,draJ,decJ,ddecJ,raH,draH,decH,ddecH,raKs,draKs,decKs,ddecKs,J,dJ,H,dH,Ks,dKs,x,y
-    ax.scatter(dic_chips['chip_%s'%(chip)][:,6],(dic_chips['chip_%s'%(chip)][:,7]),alpha=0.2)
-    np.savetxt(pruebas+'chunk_chip%s.txt'%(chip+1),dic_chips['chip_%s'%(chip)])
+
+#a ,d , m, dm, f, df,x,y,dx,dy=np.loadtxt(tmp+'stars_calibrated_'+band+'_chip'+strn(chip)+'_sirius.txt',unpack=True)
+brick=np.loadtxt(tmp+'stars_calibrated_'+band+'_chip'+str(chip)+'_sirius.txt')
+#We have to add the coordinates offset between the two lists
+#I this case I have choose this bu coomparing in Aladin
+if band=='H' and chip==3:
+    ##########################################################
+
+        
+    xm_ref=746*0.5 # xm_ref is GNS
+    ym_ref=988*0.5
+
+    xm=1007
+    ym=280
+
+    xoff = xm_ref - xm
+    yoff = ym_ref - ym
+    ##########################################################
+elif band=='H' and chip==2:
+    ##########################################################
+   
+  
+ 
+    xm_ref=1096.52 *0.5 # xm_ref is GNS
+    ym_ref=  843.433*0.5
+
+    xm=1185.6673584
+    ym=2395.6433105
+
+    xoff = xm_ref - xm
+    yoff = ym_ref - ym
+    ##########################################################
+
+print(' #'*20,'\n','xoff=%s yoff=%s'%(xoff,yoff),'\n'+' #'*20)    
+brick[:,6]+=xoff
+brick[:,7]+=yoff
 
 
 # In[4]:
 
 
-#eliminates stars overlapping borders of chips(stars closer than 0.5'')
-diff=[]
-distancia=5
-index=[]
-c=1
-for b in range(c,5):
-    for j in range(c+1,5):
-        diff=[]
-        index=[]
-        for i in range(len(dic_chips['chip_%s'%(c)])): #compara las distancia entre los puntos y guarda las menores que a, si hay mas de dos puntos con distancias menores que a, guarda la más perqueña
-            #dist=distance.cdist(dic_chips['chip_2'][i:i+1,6:8],dic_chips['chip_1'][:,6:8], 'euclidean')
-            dist=distance.cdist(dic_chips['chip_%s'%(c)][i:i+1,6:8],dic_chips['chip_%s'%(j)][:,6:8], 'euclidean')
-            d=np.where(dist<distancia)
-            if len(d[1])==1:
-                index.append(int(d[1][[np.argmin(dist[d])]]))
-                diff.append((dic_chips['chip_%s'%(c)][i],dic_chips['chip_%s'%(j)][d[1][np.argmin(dist[d])]]))
-            elif len(d[1])>1:
-                index.append(int(d[1][[np.argmin(dist[d])]]))
-                index.append(int(d[1][[np.argmax(dist[d])]]))
-                diff.append((dic_chips['chip_%s'%(c)][i],dic_chips['chip_%s'%(j)][d[1][np.argmin(dist[d])]]))
-        dic_chips['chip_%s'%(j)]=np.delete(dic_chips['chip_%s'%(j)],index,axis=0)
-        print('Comunes y eliminadas de chip %s y chip %s ---->'%(b,j),len(diff),len(index))
-    c+=1
 
-
-# In[5]:
-
-
-#make plot to check eliminated stars
-fig,ax=plt.subplots(1,1,figsize=(20,10))
-for chip in range(1,5):
-    ax.scatter(dic_chips['chip_%s'%(chip)][:,6],dic_chips['chip_%s'%(chip)][:,7],alpha=0.2)
-    
-
-
-# In[6]:
-
-
-#load field18 and add XY coordinates using astrometric information from field18_c3.fits (this images is from ESO fase 3)
-field18=np.loadtxt(GNS+'catalogue_F18_c3.txt')
-print(len(field18))
-valid=np.where(field18[:,r_gns]>0)
-field18=field18[valid]
-print(len(field18))
-x_gns=[]
-y_gns=[]
-f = fits.open(GNS+'field18_c3.fits')
-w = WCS(f[0].header)
-print(w)
-x_gns,y_gns=w.world_to_pixel_values(field18[:,r_gns],field18[:,d_gns])
-field18=np.c_[field18,x_gns,y_gns]
-
-
-# In[7]:
-
-
-#bric=[]
-#brick=np.concatenate((dic_chips['chip_1'],dic_chips['chip_2'],dic_chips['chip_3'],dic_chips['chip_4']),axis=0)
-#fig,ax=plt.subplots(1,1,figsize=(20,10))
-#ax.scatter(brick[:,6],brick[:,7],alpha=0.2)
-
-
-# In[8]:
-
-
-brick_all=[]
-brick_all=np.concatenate((dic_chips['chip_1'],dic_chips['chip_2'],dic_chips['chip_3'],dic_chips['chip_4']),axis=0)
 per=10# choose values with uncertainty in position smoller than 0.7 * mean(dx)
 #valid=np.where((brick_all[:,8]<np.mean(brick_all[:,8])*per)&(brick_all[:,9]<np.mean(brick_all[:,9])*per))
-valid=np.where((brick_all[:,8]<0.013)&(brick_all[:,9]<0.013))#0.013pixels cooresponding to a distant of 2mas
-brick=brick_all[valid]
-print(len(brick),len(brick_all))
+#valid=np.where((brick[:,8]<0.013)&(brick[:,9]<0.013))#0.013pixels cooresponding to a distant of 2mas
+#brick=brick_all[valid]
+#print(len(brick),len(brick_all))
 ##########################################################
-lon,lat=w.pixel_to_world_values(brick_all[:,6],brick_all[:,7])
-brick_all[:,0]=lon
-brick_all[:,1]=lat
-np.savetxt(pruebas+'zoc_alig_GNS_%s.txt'%(band),brick_all)
+
 ##########################################################
 #now we are looping with a degree 1 ,2,...,
 distancia=1
-for degree in range(1,3):
-    for loop in range(5):
-        print(degree)
-        brick=brick_all[valid]
-        print(len(brick))
+ciclo=5
+for degree in range(1,3):#Using degree 3 polynomial doesnt seem to improve things
+    for loop in range(1,ciclo+1):
+        print('Degree %s,iteration %s'%(degree,loop))
         diff=[]
-        for i in range(len(field18)): #compara las distancia entre los puntos y guarda las menores que a, si hay mas de dos puntos con distancias menores que a, guarda la más perqueña
-            dist=distance.cdist(field18[i:i+1,-2:],brick[:,6:8], 'euclidean')
+        for i in range(len(gns_xy)): #compara las distancia entre los puntos y guarda las menores que a, si hay mas de dos puntos con distancias menores que a, guarda la más perqueña
+            dist=distance.cdist(gns_xy[i:i+1,0:2],brick[:,6:8], 'euclidean')
             d=np.where(dist<distancia)
             if len(d[1])>0:
-                diff.append((field18[i],brick[d[1][np.argmin(dist[d])]]))
+                diff.append((field12[i],brick[d[1][np.argmin(dist[d])]]))
         print('comunes listas %s y %s ----> '%('brick','GNS'),len(diff))
         #diff=[diff[j] for j in range(len(diff)) if abs(diff[j][0][brillo]-diff[j][1][2])<l_mag]
         #print(len(diff))
@@ -204,8 +129,8 @@ for degree in range(1,3):
         y1=[]
         y2=[]
         for j in range(len(diff)):
-            x1.append(diff[j][0][-2])
-            y1.append(diff[j][0][-1])
+            x1.append(diff[j][0][0])
+            y1.append(diff[j][0][2])
             x2.append(diff[j][1][6])
             y2.append(diff[j][1][7])
 
@@ -219,33 +144,43 @@ for degree in range(1,3):
         #degree=1
         Kx,Ky=polywarp(x1,y1,x2,y2,degree=degree)
         #print(Kx[0,0])
-        xi=np.zeros(len(brick_all))
-        yi=np.zeros(len(brick_all))
+        xi=np.zeros(len(brick))
+        yi=np.zeros(len(brick))
         x=[]
         y=[]
-        x=brick_all[:,6]
-        y=brick_all[:,7]
+        x=brick[:,6]
+        y=brick[:,7]
         x=np.array(x)
         y=np.array(y)
         for k in range(degree+1):
             for m in range(degree+1):
                 xi=xi+Kx[k,m]*x**k*y**m
                 yi=yi+Ky[k,m]*x**k*y**m
-        brick_all[:,6]=xi
-        brick_all[:,7]=yi
+        brick[:,6]=xi
+        brick[:,7]=yi
+    ciclo+=35
 ##########################################################
-distancia=.4
+gns_txt=[diff[i][0][0:] for i in range(len(diff))]
+zoc_txt=[diff[i][1][0:] for i in range(len(diff))]
+np.savetxt(tmp+'GNS_commons_w_Zoc_c%s'%(chip),gns_txt,header='a ,d , m, dm, f, df,x,y,dx,dy. X and Y are the correspondig coorinates wit GNS, They are not the original ones!!!!')
+np.savetxt(tmp+'Zoc_c%s_commons_w_GNS.txt'%(chip),zoc_txt,header='x_gns, dx_gns, y_gns, dy_gns, raH, draH, decH, ddecH, mJ, dmJ, mH, dmH, mK, dmK')
+
+
+# In[5]:
+
+
+distancia=1
 diff=[]
-for i in range(len(field18)): #compara las distancia entre los puntos y guarda las menores que a, si hay mas de dos puntos con distancias menores que a, guarda la más perqueña
-    dist=distance.cdist(field18[i:i+1,-2:],brick_all[:,6:8], 'euclidean')
+for i in range(len(gns_xy)): #compara las distancia entre los puntos y guarda las menores que a, si hay mas de dos puntos con distancias menores que a, guarda la más perqueña
+    dist=distance.cdist(gns_xy[i:i+1,0:2],brick[:,6:8], 'euclidean')
     d=np.where(dist<distancia)
     if len(d[1])>0:
-        diff.append((field18[i],brick_all[d[1][np.argmin(dist[d])]]))
+        diff.append((gns_xy[i],brick[d[1][np.argmin(dist[d])]]))
 diff=np.array(diff)
-l_mag=0.6
+l_mag=1
 
-x_shift=[(diff[t][0][-2]-diff[t][1][6]) for t in range(len(diff)) if abs(diff[t][0][brillo]-diff[t][1][2])<l_mag]
-y_shift=[(diff[t][0][-1]-diff[t][1][7]) for t in range(len(diff)) if abs(diff[t][0][brillo]-diff[t][1][2])<l_mag]
+x_shift=[(diff[t][0][0]-diff[t][1][6]) for t in range(len(diff))]# if abs(diff[t][0][brillo]-diff[t][1][2])<l_mag]
+y_shift=[(diff[t][0][1]-diff[t][1][7]) for t in range(len(diff))]# if abs(diff[t][0][brillo]-diff[t][1][2])<l_mag]
 print(len(x_shift))
 
 x_shift=np.array(x_shift)*0.106
@@ -256,13 +191,13 @@ nam=['x diff (arcsec)','y diff (arcsec)']
 #ax[0]=plt.suptitle('Sigma Threshold at reconstruct = %s, CHIP  %s'%(th,ch),fontsize=20)
 #plt.clf()
 for h in range(len(ls)):
-    sig_h=sigma_clipped_stats(ls[h],sigma=2,maxiters=20,cenfunc='mean')
-    ax[h].hist(ls[h], bins=10,alpha=0.7, rwidth=0.85,color='g')
+    sig_h=sigma_clipped_stats(ls[h],sigma=3,maxiters=20,cenfunc='mean')
+    ax[h].hist(ls[h], bins=10,alpha=0.7, rwidth=1,color='g',edgecolor='black',linewidth=2)
     #ax[h].axvline(np.mean(ls[h]), color='r', linestyle='dashed', linewidth=3)
     ax[h].axvline(sig_h[0], color='r', linestyle='dashed', linewidth=3)
     ax[h].grid(axis='both', alpha=0.75)
     #ax[h].legend(['Chip%s: mean= %.4f, std=%.4f'%(1,np.mean(ls[h]),np.std(ls[h]))],fontsize=20,markerscale=0,shadow=True,loc=3,handlelength=0)
-    ax[h].legend([' dis=%s, #%s, mean= %.4f, std=%.4f'%(distancia,len(x_shift),sig_h[0],sig_h[2])],fontsize=20,markerscale=0,shadow=True,loc=3,handlelength=-0.0)
+    ax[h].legend(['Chip=%s,dmax=%spix, #%s, mean= %.4f, std=%.4f'%(chip,distancia,len(x_shift),sig_h[0],sig_h[2])],fontsize=15,markerscale=0,shadow=True,loc=3,handlelength=-0.0)
     #ax[h].legend([' #stars %s'%(len(x_shift))],fontsize=20,markerscale=0,shadow=True,loc=2,handlelength=0)
     ax[h].set_xlabel(nam[h],fontsize=20)
     ax[h].set_ylabel('# stars',fontsize=20)
@@ -273,38 +208,112 @@ fig.text(0.5, 0, 'Difference in position for common stars to GNS, band %s. Degr=
 # In[10]:
 
 
-distancia=0.3
+# In[ ]:
+
+
+
+
+
+# In[6]:
+
+
+from scipy.interpolate import fitpack
+from scipy import interpolate
+#x=[(diff[t][0][-2]) for t in range(len(diff))]
+#y=[(diff[t][0][-1]) for t in range(len(diff))]
+
+z=np.sqrt(np.array(x_shift)**2+np.array(y_shift)**2)
+x=[(diff[t][0][0]) for t in range(len(diff))]
+y=[(diff[t][0][1]) for t in range(len(diff))]
+xline = np.linspace(min(x), max(x), 2000)
+yline = np.linspace(min(y), max(y), 2000)
+#xline = np.linspace(0, 2548, 2549)
+#yline = np.linspace(0, 2548, 2549)
+xgrid,ygrid = np.meshgrid(xline, yline)
+#tck=fitpack.bisplrep(xs,ys,zs,s=len(ys)-2*np.sqrt(len(ys)))
+tck=fitpack.bisplrep(x,y,z,kx=3,ky=3)
+#znew = interpolate.bisplev(xnew, ynew, tck)
+znew = interpolate.bisplev(xline, yline, tck)
+
+plt.figure()
+#plt.pcolormesh(xnew, ynew, znew, shading='flat',vmin=min(zs), vmax=max(zs))
+#plt.pcolormesh(xnew, ynew, znew)
+plt.pcolormesh(xgrid, ygrid, znew)
+plt.colorbar()
+plt.title("Interpolated function.")
+plt.show()
+
+
+# In[7]:
+
+
+#x_gns, dx_gns, y_gns, dy_gns, raH, draH, decH, ddecH, mJ, dmJ, mH, dmH, mK, dmK=np.loadtxt(GNS+'field12_on_brick.txt',unpack=True)
+#field12=np.loadtxt(GNS+'field12_on_brick.txt')
+#a ,d , m, dm, f, df,x,y,dx,dy=np.loadtxt(tmp+'stars_calibrated_'+band+'_chip'+strn(chip)+'_sirius.txt',unpack=True)
+#brick=np.loadtxt(tmp+'stars_calibrated_'+band+'_chip'+str(chip)+'_sirius.txt')
+sig=2
+    
+distancia=1
 diff=[]
-for i in range(len(field18)): #compara las distancia entre los puntos y guarda las menores que a, si hay mas de dos puntos con distancias menores que a, guarda la más perqueña
-    dist=distance.cdist(field18[i:i+1,-2:],brick_all[:,6:8], 'euclidean')
+for i in range(len(gns_xy)): #compara las distancia entre los puntos y guarda las menores que a, si hay mas de dos puntos con distancias menores que a, guarda la más perqueña
+    dist=distance.cdist(gns_xy[i:i+1,0:2],brick[:,6:8], 'euclidean')
     d=np.where(dist<distancia)
     if len(d[1])>0:
-        diff.append((field18[i],brick_all[d[1][np.argmin(dist[d])]]))
+        diff.append((field12[i],brick[d[1][np.argmin(dist[d])]]))
+
+resta=[]   
+mags=[]
+for i in range(len(diff)):
+    #print(diff[i][0][2]-diff[i][1][2])
+    resta.append(diff[i][0][10]-diff[i][1][2])
+    mags.append((diff[i][0][10],diff[i][1][2]))
+    
 diff=np.array(diff)
-l_mag=1
+resta=np.array(resta)
+average=np.mean(np.array(resta))
+mags=np.array(mags)
 
-x_shift=[(diff[t][0][-2]-diff[t][1][6]) for t in range(len(diff)) if abs(diff[t][0][brillo]-diff[t][1][2])<l_mag]
-y_shift=[(diff[t][0][-1]-diff[t][1][7]) for t in range(len(diff)) if abs(diff[t][0][brillo]-diff[t][1][2])<l_mag]
-print(len(x_shift))
+s=sigma_clipped_stats(resta,sigma=sig,maxiters=10)
+mask_sig=sigma_clip(resta,sigma=sig,maxiters=10)
+nope=np.where(mask_sig.mask==True)
 
-x_shift=np.array(x_shift)*0.106
-y_shift=np.array(y_shift)*0.106
-fig,ax= plt.subplots(1,2,figsize=(20,10))
-ls=[x_shift,y_shift]
-nam=['x diff (arcsec)','y diff (arcsec)']
-#ax[0]=plt.suptitle('Sigma Threshold at reconstruct = %s, CHIP  %s'%(th,ch),fontsize=20)
-#plt.clf()
-for h in range(len(ls)):
-    sig_h=sigma_clipped_stats(ls[h],sigma=2,maxiters=20,cenfunc='mean')
-    ax[h].hist(ls[h], bins=10,alpha=0.7, rwidth=0.85,color='g')
-    #ax[h].axvline(np.mean(ls[h]), color='r', linestyle='dashed', linewidth=3)
-    ax[h].axvline(sig_h[0], color='r', linestyle='dashed', linewidth=3)
-    ax[h].grid(axis='both', alpha=0.75)
-    #ax[h].legend(['Chip%s: mean= %.4f, std=%.4f'%(1,np.mean(ls[h]),np.std(ls[h]))],fontsize=20,markerscale=0,shadow=True,loc=3,handlelength=0)
-    ax[h].legend([' dis=%s,#%s, mean= %.4f, std=%.4f'%(distancia,len(x_shift),sig_h[0],sig_h[2])],fontsize=20,markerscale=0,shadow=True,loc=3,handlelength=-0.0)
-    #ax[h].legend([' #stars %s'%(len(x_shift))],fontsize=20,markerscale=0,shadow=True,loc=2,handlelength=0)
-    ax[h].set_xlabel(nam[h],fontsize=20)
-    ax[h].set_ylabel('# stars',fontsize=20)
-    ax[h].tick_params(axis='x', labelsize=20)
-    ax[h].tick_params(axis='y', labelsize=20)
-fig.text(0.5, 0, 'Difference in position for common stars to GNS, band %s. Degr=%s'%(band,degree),fontsize=20, ha='center')
+nbins=(np.ceil(max(mags[:,1]))-np.floor(min(mags[:,1])))
+mags_bins=np.floor(min(mags[1]))+np.arange(nbins)
+
+fig,ax=plt.subplots(1,1,figsize=(20,10))
+ax.scatter(mags[:,1],resta,color='k',alpha=0.2)
+ma=mags[:,1]
+ax.scatter(mags[:,1][nope],resta[nope],color='red',marker='x',s=100,alpha=0.7)
+ax.grid()
+ax.axhline(s[0],color='g',ls='--',lw=2)
+ax.set_ylim(-2,2)
+ax.tick_params(axis='x', labelsize=20)
+ax.tick_params(axis='y', labelsize=20)
+ax.legend(['Chip %s,Common stars #%s'%(chip,len(mags[:,1]))],fontsize=20,shadow=True,loc=1, handlelength=0,handletextpad=0)
+ax.set_xlabel('$[%s]_{Zoc}$'%(band),fontsize=20)
+ax.set_ylabel('$[%s]_{Zoc}-[%s]_{SIR}$'%(band,band),fontsize=20)
+#ax[i].set_xlim(x0,x1)
+mmag =np.zeros(shape=(int(nbins)))
+sig_mag =np.zeros(shape=(int(nbins)))
+for j in range(int(nbins)):
+    thisbin=np.where((mags[:,1]>mags_bins[j])&(mags[:,1]<=mags_bins[j]+1))
+    vals = resta[thisbin]
+    nope_thisbin=np.where((mags[:,1][nope]>mags_bins[j])&(mags[:,1][nope]<mags_bins[j]+1))
+    if len(vals)>1:
+        bin_rej=sigma_clip(vals, sigma=2, maxiters=5,masked=True)
+        sig_bin=sigma_clipped_stats(vals,sigma=2.0,maxiters=5)
+        mmag[j]=sig_bin[0]
+        sig_mag[j]=sig_bin[2]
+    ax.errorbar(mags_bins[j]+0.5,mmag[j],sig_mag[j],color='red', elinewidth=3,capsize=10,capthick=2,barsabove=True,zorder=3)
+    ax.text(min(mags[:,1])+1,1,'mean offset (%s$\sigma$) =%.3f, std= %.3f'%(sig,s[0],s[2]),color='green',fontsize=14) 
+    ax.text(mags_bins[j]+0.4,-1.5,'%.3f'%(sig_mag[j]),color='red',fontsize=14)  
+    ax.text(mags_bins[j]+0.4,-1.7,'%s'%(len(vals)),color='blue',fontsize=14)
+    ax.text(mags_bins[j]+0.4,-1.9,'%s'%(len(nope_thisbin[0])),color='orange',fontsize=14)
+
+
+
+# In[ ]:
+
+
+
+
