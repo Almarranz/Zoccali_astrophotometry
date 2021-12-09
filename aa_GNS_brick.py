cef3430d27f2bc0dcd1f74dcf205ac6747d0a0e2
Alvaro Martinez <amartinez@iaa.es>
2021-12-09 13:30:43 +0100

    some chenges

--- aa_GNS_brick.py
+++ aa_GNS_brick.py
@@ -14,8 +14,8 @@ from astropy.io import fits
 from scipy.spatial import distance
 import sys
 folder='im_jitter_NOgains/'
-chip=3
-zone='Z1'
+# chip=4
+zone='Z2'
 
 #%%
 #%%
@@ -31,8 +31,8 @@ if in_brick==1:
             x_gns, dx_gns, y_gns, dy_gns, raH, draH, decH, ddecH, mJ, dmJ, mH, dmH, mK, dmK=np.loadtxt(GNS+'field12_on_brick_reduced.txt',unpack=True)
 
 elif in_brick==0:
-    field=16# field 16(lst 2 ,3) field3(lst 1,4)
-    lst=3
+    field=3# field 16(lst 2 ,3) field3(lst 1,4)
+    lst=4
     GNS='/Users/amartinez/Desktop/PhD/HAWK/The_Brick/field%s/'%(field)
     # x_gns, dx_gns, y_gns, dy_gns, raH, draH, decH, ddecH, mJ, dmJ, mH, dmH, mK, dmK=np.loadtxt(GNS+'cat_Ban_%s_%s.txt'%(field,lst),unpack=True)
     x_gns, dx_gns, y_gns, dy_gns, raH, draH, decH, ddecH, mJ, dmJ, mH, dmH, mK, dmK=np.loadtxt(GNS+'%s_cat_Ban_%s_%s.txt'%(zone,field,lst),unpack=True)#'Z1_..' hace referencia a una lista sobrre una zona del mismo tamaño de Zone A sobre el brock
@@ -78,7 +78,7 @@ elif in_brick==0:
  # -638.350, 215.718 manually stimated xoff yoff
 check_x,check_y=2,2
 while abs(check_x) >1 or abs(check_y)>1  :
-    m,(_,_)= aa.find_transform(zoca, GNS_xy,max_control_points=200)
+    m,(_,_)= aa.find_transform(zoca, GNS_xy,max_control_points=250)
     print('For fiel%s, chip%s'%(field,lst)+'\n'+"Translation: (x, y) = (%.2f, %.2f)"%(m.translation[0],m.translation[1]))
     print("Rotation: %.3f degrees"%(m.rotation * 180.0 / np.pi))
     print("Scale factor: %.4f"%(m.scale))
@@ -86,22 +86,24 @@ while abs(check_x) >1 or abs(check_y)>1  :
     test_gns = aa.matrix_transform(zoca, m.params)
             
     print(20*'#'+'\n'+'CHECKING'+'\n'+20*'#')
-    t,(_,_)= aa.find_transform(test_gns,GNS_xy,max_control_points=200)
+    t,(_,_)= aa.find_transform(test_gns,GNS_xy,max_control_points=250)
     print("Translation: (x, y) = (%.2f, %.2f)"%(t.translation[0],t.translation[1]))
     print("Rotation: %.3f degrees"%(t.rotation * 180.0 / np.pi))
     print("Scale factor: %.4f"%(t.scale))
     check_x= t.translation[0]
     check_y= t.translation[1]
-print('___NOW TRANSFORMING GNS___')  
+print('___NOW TRANSFORMING ZOC___')  
 trans=aa.matrix_transform(zoca, m.params)
 zoc[:,6]=trans[:,0]
 zoc[:,7]=trans[:,1]
-if in_brick==1:
-    np.savetxt(tmp+'Z1_aa_BRICK_stars_calibrated_H_chip'+str(chip)+'_sirius.txt',zoc, fmt='%.5f') 
-elif in_brick==0:
-    np.savetxt(tmp + 'Z1_aa_stars_calibrated_H_on_field%s_%s.txt'%(field,lst),zoc, fmt='%.5f')
-
-
+# =============================================================================
+# if in_brick==1:
+#     np.savetxt(tmp+'%s_aa_BRICK_stars_calibrated_H_chip'+str(chip)+'_sirius.txt'%(zone),zoc, fmt='%.5f') 
+# elif in_brick==0:
+#     np.savetxt(tmp + '%s_aa_stars_calibrated_H_on_field%s_%s.txt'%(zone,field,lst),zoc, fmt='%.5f')
+# 
+# 
+# =============================================================================
 
 
 
